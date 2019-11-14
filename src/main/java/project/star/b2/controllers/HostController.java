b47b31b840ee351b5ed4519c74aced618c4cf681
gwonp217 <gwonp217@gmail.com>
2019-11-14 21:41:36 +0900

    [지원] HostController 추가

--- src/main/java/project/star/b2/controllers/HostController.java
+++ src/main/java/project/star/b2/controllers/HostController.java
@@ -1,5 +1,37 @@
 package project.star.b2.controllers;
 
-public class HostController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class HostController {
+	
+	/********************************************************************
+	 *  							메인
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/host/main.do", method = RequestMethod.GET)
+	public ModelAndView main() {
+		
+		return new ModelAndView("jsp/host/main");
+	}
+	
+	/********************************************************************
+	 *  							공실 등록하기
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/host/rm_add.do", method = RequestMethod.GET)
+	public ModelAndView rm_add() {
+		
+		return new ModelAndView("jsp/host/rm_add");
+	}
+	
+	/********************************************************************
+	 *  							공실관리
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/host/rmli.do", method = RequestMethod.GET)
+	public ModelAndView rmli() {
+		
+		return new ModelAndView("jsp/host/rmli");
+	}
 }
