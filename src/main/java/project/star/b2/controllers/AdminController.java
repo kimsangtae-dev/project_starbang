bda191794481d96da433a34c8ee3f425c729f63f
sumin <gju031@gmail.com>
2019-11-14 19:20:40 +0900

    [수민]AdminController 추가

--- src/main/java/project/star/b2/controllers/AdminController.java
+++ src/main/java/project/star/b2/controllers/AdminController.java
@@ -1,5 +1,82 @@
 package project.star.b2.controllers;
 
-public class AdminController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class AdminController {
+	
+	/********************************************************************
+	 *  						글쓰기 수정
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/edit.do", method = RequestMethod.GET)
+	public ModelAndView edit() {
+		
+		return new ModelAndView("jsp/admin/edit");
+	}
+	
+	/********************************************************************
+	 *  					   관리자 자주묻는질문
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/faq.do", method = RequestMethod.GET)
+	public ModelAndView faq() {
+		
+		return new ModelAndView("jsp/admin/faq");
+	}
+	
+	/********************************************************************
+	 *  						관리자 로그인
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/login.do", method = RequestMethod.GET)
+	public ModelAndView login() {
+		
+		return new ModelAndView("jsp/admin/login");
+	}
+	
+	/********************************************************************
+	 *  						관리자 메인
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/main.do", method = RequestMethod.GET)
+	public ModelAndView main() {
+		
+		return new ModelAndView("jsp/admin/main");
+	}
+	
+	/********************************************************************
+	 *  						관리자 공지사항
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/notice.do", method = RequestMethod.GET)
+	public ModelAndView notice() {
+		
+		return new ModelAndView("jsp/admin/notice");
+	}
+	
+	/********************************************************************
+	 *  							방 관리
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/rmli.do", method = RequestMethod.GET)
+	public ModelAndView rmli() {
+		
+		return new ModelAndView("jsp/admin/rmli");
+	}
+	
+	/********************************************************************
+	 *  							회원 관리
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/userli.do", method = RequestMethod.GET)
+	public ModelAndView userli() {
+		
+		return new ModelAndView("jsp/admin/userli");
+	}
+	
+	/********************************************************************
+	 *  							글쓰기
+	 *******************************************************************/
+	@RequestMapping(value = "/jsp/admin/write.do", method = RequestMethod.GET)
+	public ModelAndView write() {
+		
+		return new ModelAndView("jsp/admin/write");
+	}	
 }
