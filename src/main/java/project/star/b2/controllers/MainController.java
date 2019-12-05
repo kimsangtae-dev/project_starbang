5c0afb43990974639aaf1c2bb041d15bc793847e
gwonp217 <gwonp217@gmail.com>
2019-12-05 09:31:23 +0900

    [지원] mainController 수정

--- src/main/java/project/star/b2/controllers/MainController.java
+++ src/main/java/project/star/b2/controllers/MainController.java
@@ -1,13 +1,21 @@
 package project.star.b2.controllers;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import project.star.b2.helper.PageData;
