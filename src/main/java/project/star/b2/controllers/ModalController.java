6a59533b24441db49f38d0161f89423488123249
sangtae <air1330@naver.com>
2019-12-06 12:23:07 +0900

    [상태] 모달 컨트롤러 수정

--- src/main/java/project/star/b2/controllers/ModalController.java
+++ src/main/java/project/star/b2/controllers/ModalController.java
@@ -3,6 +3,9 @@ package project.star.b2.controllers;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -120,77 +123,62 @@ public class ModalController {
 	}
 
 	/********************************************************************
-	 * 로그인 폼
+	 *  						로그인
 	 *******************************************************************/
-
 	@RequestMapping(value = "/modal/login.do", method = RequestMethod.GET)
-	public ModelAndView login() {
-
-		return new ModelAndView("modal/login");
+	public ModelAndView login(Model model, HttpServletRequest request) {
+		
+     
+        return new ModelAndView("modal/login");
 	}
-
+	
 	/********************************************************************
-	 * 로그인
+	 *  						로그인 action폼
 	 *******************************************************************/
-	/*
-	 * @RequestMapping(value = "/modal/login_ok.do", method = RequestMethod.GET)
-	 * public ModelAndView login_ok(Model model, HttpServletRequest request) {
-	 * 
-	 *//** 1) 사용자가 입력한 파라미터 수신 및 유효성 검사 */
-	/*
-	 * String email = webHelper.getString("email"); String passwd =
-	 * webHelper.getString("passwd"); String name = webHelper.getString("name");
-	 * 
-	 * // 필수 값의 존재여부 검사 if (email == null || email.contentEquals("")) { return
-	 * webHelper.redirect(null, "아이디를 입력하세요."); }
-	 * 
-	 * if (passwd == null || passwd.contentEquals("")) { return
-	 * webHelper.redirect(null, "비밀번호를 입력하세요."); }
-	 * 
-	 *//** 2) 데이터 조회하기 */
-	/*
-	 * // 조회에 필요한 조건값(검색어)를 Beans에 담는다. User input = new User();
-	 * input.setEmail(email);
-	 * 
-	 * 
-	 * // 조회결과를 저장할 객체 선언 User output = null;
-	 * 
-	 * try { //데이터 조회 output = userService.getUserLogin(input); } catch (Exception
-	 * e) { return webHelper.redirect(null, e.getLocalizedMessage()); }
-	 * 
-	 * 
-	 * // 아이디와 비밀번호가 가입된 정보와 일치하는지 검사 // --> 여기서는 특정 문자열과 동일한 경우 가입된 아이디로 판다. if
-	 * (!email.equals(output.getEmail()) || !passwd.equals(output.getPasswd())) {
-	 * return webHelper.redirect(null, "아이디나 비밀번호가 잘못되었습니다.");
-	 * 
-	 * }
-	 * 
-	 * // WebHelper를 통해서 세션값 가져오기 Member loginInfo = (Member)
-	 * webHelper.getSession("login_info");
-	 * 
-	 * // 저장된 쿠키값 조회하기 String rememberUserId =
-	 * webHelper.getCookie("remember_user_id", "");
-	 * 
-	 *//** 1) request 객체를 사용해서 세션 객체 만들기 */
-	/*
-	 * // --> import javax.servlet.http.HttpSession; HttpSession session =
-	 * request.getSession();
-	 * 
-	 * 
-	 * // 모든 세션값을 삭제 webHelper.removeAllSession();
-	 * 
-	 *//** 2) 세션 저장, 삭제 */
-	/*
-	 * // 생성된 세션 객체를 사용하는 방법은 JSP와 동일하다. if (!email.equals("")) { // 입력 내용이 있다면 세션
-	 * 저장 처리 session.setAttribute("email", email); } else { // 입력 내용이 없는 경우 세션 삭제
-	 * session.removeAttribute("email"); }
-	 * 
-	 * 
-	 *//** 3) View 처리 *//*
-						 * model.addAttribute("output", output);
-						 * 
-						 * return new ModelAndView("index"); }
-						 */
+	@RequestMapping(value = "/modal/login_ok.do", method = RequestMethod.GET)
+	public ModelAndView login_ok(Model model, HttpServletRequest request) {
+		
+
+		// 1) 사용자가 입력한 파라미터 수신 및 필수값 검사 */
+		String email = webHelper.getString("email"); 
+		String passwd = webHelper.getString("passwd");
+			
+		
+		// 필수 값의 존재여부 검사 
+		if (email == null || email.contentEquals("")) { 
+			return webHelper.redirect(null, "아이디를 입력하세요."); }
+		
+		if (passwd == null || passwd.contentEquals("")) { 
+			return webHelper.redirect(null, "비밀번호를 입력하세요."); }
+ 
+		
+		// 2) 사용자가 입력한 값을 Beans에 저장
+		User input = new User();
+		input.setEmail(email);
+		
+		
+		// 조회결과를 저장할 객체 선언 
+		User output = null;
+		
+		try { 
+			//데이터 조회 
+		  	output = userService.getUserLogin(input); 
+		} catch (Exception e) { 
+			return webHelper.redirect(null, e.getLocalizedMessage()); }
+		
+		/* request 객체를 사용해서 세션 객체 만들기 */
+		HttpSession session = request.getSession();
+	
+		// 가입된 정보와 DB가 일치하는지 검사 후 세션 생성   
+		if ( passwd.equals(output.getPasswd())) {
+			session.setAttribute("loginInfo", output);
+		} else {	
+			return webHelper.redirect(null, "비밀번호가 잘못되었습니다.");
+		} 
+        
+      	return new ModelAndView("index");
+		
+	}
 
 	/********************************************************************
 	 * 비밀번호 찾기
