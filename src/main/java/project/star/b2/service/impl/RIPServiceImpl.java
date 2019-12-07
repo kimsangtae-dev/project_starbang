6620be968d481642960f397cabe04c9803b613e4
KIHONG-KIM <frozenorbs@naver.com>
2019-12-08 01:13:31 +0900

    [기홍] Room+Info+Price Service = RIPService.java 업로드

---
+++ src/main/java/project/star/b2/service/impl/RIPServiceImpl.java
@@ -0,0 +1,137 @@
+package project.star.b2.service.impl;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import project.star.b2.model.Info;
+import project.star.b2.model.Price;
+import project.star.b2.model.Room;
+import project.star.b2.service.RIPService;
+
+
+
+/** 학과 데이터 관리 기능을 제공하기 위한 Service 계층에 대한 구현체 */
+@Slf4j
+@Service
+public class RIPServiceImpl implements RIPService {
+
+	/** MyBatis */
+    // --> import org.apache.ibatis.session.SqlSession
+	@Autowired
+    SqlSession sqlSession;
+	
+    /**
+     * 학과 데이터 등록하기
+     * @param Room 저장할 정보를 담고 있는 Beans
+     * @throws Exception
+     */
+    @Override
+    public int addRoom(Room input) throws Exception {
+        int result = 0;
+
+        try {
+            result = sqlSession.insert("RoomMapper.insertItem", input);
+            
+            if (result == 0) {
+                throw new NullPointerException("result=0");
+            }
+        } catch (NullPointerException e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("저장된 데이터가 없습니다.");
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("데이터 저장에 실패했습니다.");
+        }
+
+        return result;
+    }
+
+
+	@Override
+	public int addInfo(Info input) throws Exception {
+		int result = 0;
+
+        try {
+            result = sqlSession.insert("InfoMapper.insertItem", input);
+            
+            if (result == 0) {
+                throw new NullPointerException("result=0");
+            }
+        } catch (NullPointerException e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("저장된 데이터가 없습니다.");
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("데이터 저장에 실패했습니다.");
+        }
+
+        return result;
+	}
+	
+	@Override
+	public int addPrice(Price input) throws Exception {
+		int result = 0;
+		
+		try {
+			result = sqlSession.insert("PriceMapper.insertItem", input);
+			
+			if (result == 0) {
+				throw new NullPointerException("result=0");
+			}
+		} catch (NullPointerException e) {
+			log.error(e.getLocalizedMessage());
+			throw new Exception("저장된 데이터가 없습니다.");
+		} catch (Exception e) {
+			log.error(e.getLocalizedMessage());
+			throw new Exception("데이터 저장에 실패했습니다.");
+		}
+		
+		return result;
+	}
+
+    @Override
+    public Room getRoomItem(Room input) throws Exception {
+        Room result = null;
+
+        try {
+            result = sqlSession.selectOne("RoomMapper.selectItem", input);
+
+            if (result == null) {
+                throw new NullPointerException("result=null");
+            }
+        } catch (NullPointerException e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("조회된 데이터가 없습니다.");
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("데이터 조회에 실패했습니다.");
+        }
+
+        return result;
+    }
+
+
+    @Override
+    public Info getInfoItem(Info input) throws Exception {
+        Info result = null;
+
+        try {
+            result = sqlSession.selectOne("InfoMapper.selectItem", input);
+
+            if (result == null) {
+                throw new NullPointerException("result=null");
+            }
+        } catch (NullPointerException e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("조회된 데이터가 없습니다.");
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            throw new Exception("데이터 조회에 실패했습니다.");
+        }
+
+        return result;
+    }
+	
+}
