5ea5a1dc70266f882e8300250b765caeb756494a
sum0725 <gju031@gmail.com>
2019-12-10 10:44:47 +0900

    [수민] 쿠키를 list로 받게 수정

--- src/main/java/project/star/b2/service/GalleryService.java
+++ src/main/java/project/star/b2/service/GalleryService.java
@@ -14,44 +14,44 @@ public interface GalleryService {
      * @throws Exception
      */
     public List<Gallery> getGalleryList(Gallery input) throws Exception;
-    
+
     /**
      * 갤러리 데이터가 저장되어 있는 갯수 조회
      * @return int
      * @throws Exception
      */
     public int getGalleryCount(Gallery input) throws Exception;
-    
+
     /**
      * 갤러리 데이터 - region_2depth_name(구) 목록 조회
      * @return 조회 결과에 대한 컬렉션
      * @throws Exception
      */
     public List<Gallery> getGalleryGuList(Gallery input) throws Exception;
-    
+
     /**
      * 갤러리 데이터 - region_2depth_name(구) 개수 조회
      * @return 조회 결과에 대한 컬렉션
      * @throws Exception
      */
     public int getGalleryGuCount(String input) throws Exception;
-    
+
     /**
      * 학과 데이터 목록 조회
      * @return 조회 결과에 대한 컬렉션
      * @throws Exception
      */
     public List<Gallery> getRoomPositionList(Gallery input) throws Exception;
-    
+
     /**
      * @param 인기매물에 대한 파라미터
      * @return
      * @throws Exception
      */
     public List<Popular> getPopularGalleryList(Popular input) throws Exception;
-    
+
     public int getGalleryCount(Popular input) throws Exception;
-    
-    public List<Gallery> getCookieList(Gallery input) throws Exception;
-    
+
+    public List<Gallery> getCookieList(List<String> input) throws Exception;
+
 }
