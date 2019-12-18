f892be1c6428e97fd95bcebc5c44ca37f811bb12
sum0725 <gju031@gmail.com>
2019-12-18 11:33:33 +0900

    [수민]쿠키 역순정렬 구문 추가

--- src/main/java/project/star/b2/controllers/MainController.java
+++ src/main/java/project/star/b2/controllers/MainController.java
@@ -200,62 +200,62 @@ public class MainController {
 	 * 상세페이지 (rmdt 파라미터 읽기)
 	 *******************************************************************/
 	@RequestMapping(value = "/main/rmdt.do", method = RequestMethod.GET)
-	public ModelAndView rmdt(Model model, 
+	public ModelAndView rmdt(Model model,
 			@RequestParam(value = "roomno", defaultValue = "") int roomno) {
-		
+
 		Room input_room = new Room();
 		input_room.setRoomno(roomno);
-		
+
 		Info input_info = new Info();
 		input_info.setRoomno(roomno);
-		
+
 		Price input_price = new Price();
 		input_price.setRoomno(roomno);
-		
+
 		UploadItem input_image = new UploadItem();
 		input_image.setRoomno(roomno);
-		
+
 		User input_user = new User();
-		
+
 		Room output_room = null;
 		Info output_info = null;
 		List<Price> output_price = null;
 		List<UploadItem> output_image = null;
 		User output_user =null;
-		
+
 		try {
-			
+
 			output_room = roomService.getRoomItem(input_room);
 			log.info("성공 roomService");
-			
+
 			output_info = infoService.getInfoItem(input_info);
 			log.info("성공 infoService");
-			
+
 			output_price = priceService.getPriceList_by_roomno(input_price);
 			log.info("성공 priceService");
-			
+
 			output_image = uploadService.getImageList_by_roomno(input_image);
 			log.info("성공 uploadService");
-			
+
 			input_user.setUserno(output_room.getUserno());
 			output_user = userService.getUserItem(input_user);
-			
+
 		} catch(Exception e) {
-			
+
 			log.debug("방 조회에 실패하였습니다.");
 			log.error(e.getLocalizedMessage());
 			return webHelper.redirect(null, e.getLocalizedMessage());
-			
+
 		}
-		
+
 		/** view 화면으로 보여주기 */
 		model.addAttribute("room", output_room);
 		model.addAttribute("info", output_info);
 		model.addAttribute("price", output_price);
 		model.addAttribute("img", output_image);
 		model.addAttribute("user", output_user);
-		
-		
+
+
 		return new ModelAndView("main/rmdt");
 	}
 
@@ -306,6 +306,7 @@ public class MainController {
 		try {
 			// 쿠키로 저장된 방번호로 조회
 			output = galleryService.getCookieList(list);
+			Collections.reverse(output);
 		} catch (Exception e) {
 			return new ModelAndView("main/rtrm");
 		}
@@ -369,7 +370,7 @@ public class MainController {
 		if (sizeTo == 999999) {
 			filter.setSizeTo(115);
 		} else {
-			filter.setSizeTo(sizeTo);			
+			filter.setSizeTo(sizeTo);
 		}
 
 		/** 2) 데이터 조회하기 */
@@ -529,21 +530,21 @@ public class MainController {
 	 *******************************************************************/
 	@RequestMapping(value = "main/repwd.do")
 	public String repwd(Model model, HttpServletRequest request) {
-	    
+
 	    /** pwd에서 입력한 이메일 세션 생성*/
 	    HttpSession session = request.getSession();
 	    String email = (String) session.getAttribute("fullemail");
-	     
+
 	    model.addAttribute("fullemail", email);
 	    session.invalidate();
-	      
+
 	    return "main/repwd";
 	}
-	   
+
 	/********************************************************************
 	 * 비밀번호 찾기 ok
 	 *******************************************************************/
-	   
+
 	/** 수정 폼에 대한 action 페이지 */
 	@RequestMapping(value = "main/repwd_ok.do", method = RequestMethod.POST)
 	public ModelAndView edit(Model model) {
@@ -590,6 +591,6 @@ public class MainController {
 
 		return (List<String>) new ModelAndView("main/search");
 	}
-	
+
 
 }
