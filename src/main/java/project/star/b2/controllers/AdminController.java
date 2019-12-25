0a5952fa0b092272813363982581fb5974b631ab
ayyb <narabyeol@gmail.com>
2019-12-26 00:06:16 +0900

    [명섭]admin 페이지 쓰이지 않는 코드 리팩토링 처리

--- src/main/java/project/star/b2/controllers/AdminController.java
+++ src/main/java/project/star/b2/controllers/AdminController.java
@@ -503,58 +503,6 @@ public class AdminController {
 	}
 
 	/********************************************************************
-	 * 체크된 확인매울!
-	 *******************************************************************/
-	@RequestMapping(value = "/admin/rmli2.do", method = RequestMethod.GET)
-	public ModelAndView rmli2(Model model) {
-		/** 1) 필요한 변수값 생성 */
-		String keyword = webHelper.getString("keyword", ""); // 검색어
-		int keyword2 = webHelper.getInt("keyword", 0); // 검색어
-		int nowPage = webHelper.getInt("page", 1); // 페이지 번호(기본값 1)
-		int totalCount = 0; // 전체 게시글 수
-		int listCount = 10; // 한 페이지당 표시할 목록 수
-		int pageCount = 5; // 한 그룹당 표시할 페이지 번호 수
-
-		/** 2) 데이터 조회하기 */
-		// 조회에 필요한 조건값(검색어)를 Beans에 담는다.
-		Room input = new Room();
-		input.setRoomtype(keyword);
-		input.setTitle(keyword);
-		input.setArea(keyword2);
-		input.setFee(keyword2);
-		input.setConfirmdate(keyword);
-		input.setAddress(keyword);
-		input.setUserno(keyword2);
-		input.setName(keyword);
-		/* input.setHidden(keyword); */
-
-		List<Room> output = null; // 조회결과가 저장될 객체
-		PageData pageData = null; // 페이지 번호를 계산할 결과가 저장될 객체
-
-		try {
-			// 전체 게시글 수 조회
-			totalCount = roomService.getRoomCount(input);
-			// 페이지 번호 계산 --> 계산결과를 로그로 출력될 것이다.
-			pageData = new PageData(nowPage, totalCount, listCount, pageCount);
-
-			// SQL의 LIMIT절에서 사용될 값을 Beans의 static 변수에 저장
-			Room.setOffset(pageData.getOffset());
-			Room.setListCount(pageData.getListCount());
-			// 데이터 조회하기
-			output = roomService.getRoomCheckConfirmList(input);
-		} catch (Exception e) {
-			return webHelper.redirect(null, e.getLocalizedMessage());
-		}
-
-		/** 3)View 처리 */
-		model.addAttribute("keyword", keyword);
-		model.addAttribute("output", output);
-		model.addAttribute("pageData", pageData);
-
-		return new ModelAndView("admin/rmli");
-	}
-
-	/********************************************************************
 	 * 관리자 로그인
 	 *******************************************************************/
 	@RequestMapping(value = "/admin/lg.do", method = RequestMethod.GET)
