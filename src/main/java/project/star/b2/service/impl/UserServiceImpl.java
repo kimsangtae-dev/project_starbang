faa135e9ff67f02dc8d33d0be2e5b937f09cd0ab
dkalsh <wkqk89@gmail.com>
2019-12-27 12:25:35 +0900

    [수진]UserServiceImpl 삭제구현

--- src/main/java/project/star/b2/service/impl/UserServiceImpl.java
+++ src/main/java/project/star/b2/service/impl/UserServiceImpl.java
@@ -302,4 +302,25 @@ public class UserServiceImpl implements UserService {
 		return result;
 	}
 	
+	/** 회원 탈퇴 이유 */
+	@Override
+	public int outUserinsert(User input) throws Exception {
+		int result = 0;
+
+		try {
+			result = sqlSession.update("UserMapper.outUserinsert", input);
+
+			if (result == 0) {
+				throw new NullPointerException("result=0");
+			}
+		} catch (NullPointerException e) {
+			log.error(e.getLocalizedMessage());
+			throw new Exception("저장된 데이터가 없습니다.");
+		} catch (Exception e) {
+			log.error(e.getLocalizedMessage());
+			throw new Exception("데이터 저장에 실패했습니다.");
+		}
+		return result;
+	}
+	
 }
